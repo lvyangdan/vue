297b28d6e4ec85d4faeb8f11ca4133c0bf813e64
lvyangdan <2737920271@qq.com>
2017-08-07 16:01:06 +0800

    third

--- vue/src/components/CommentBox.vue
+++ vue/src/components/CommentBox.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="comment-box">
     <ul>
-      <li v-for="comment in comments">
-        {{ comment.text }}
+      <li v-for="news in comments">
+        {{ news.text.split('').reverse().join('')}}
       </li>
+      <li>{{state ? 'yes':'no'}}</li>
     </ul>
+    <p v-if="seen">看不加我</p>
   </div>
 </template>
 
@@ -14,20 +16,27 @@
     data: () => ({
       comments: [
         {
-          text: 'hello git'
+          text: 'hello git study'
         },
         {
           text: 'hello vuejs'
+        },
+        {
+          text: 'newming'
         }
       ]
-    })
+    }),
+    state: 'ture',
+    data1: {
+      seen: 'true'
+    }
   }
 </script>
 
 <style scoped>
   .comment-box {
     background-color: #fff;
-    box-shadow: 0 1px 2px rgba(0, 0, 0, .5);
+    box-shadow: 0 1px 2px rgba(0, 0, 0, 0.5);
     width: 80%;
     margin: 30px auto;
     padding: 20px;
